cfc7438871caf39013e4ac59047e585a03ee388e
sbogde <sbogde@gmail.com>
2026-01-17 18:58:42 +0000

    add HousingLocationComponent to HomeComponent imports and template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';  
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
       </form>
     </section>
     <section class="results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
