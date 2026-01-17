d225b4558e5a77abc320c834157c34ce9cae76fe
sbogde <sbogde@gmail.com>
2026-01-17 19:17:53 +0000

    add input binding and update template in HousingLocationComponent

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,14 +9,16 @@ import { CommonModule } from '@angular/common';
   template: `
     <section class="listing">
       <img
-        class="listing-photo"
+        class="listing-photo" [src]="housingLocation.photo" alt="Photo of {{ housingLocation.name }}"
       />
-      <h2 class="listing-heading"></h2>
-      <p class="listing-location"></p>
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
